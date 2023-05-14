d144ca3411b26e70041be340867557a1f70823d0
fultonoff <fultonkilebe7@gmail.com>
2023-05-14 10:47:49 +0200

    ✅fetch data from google search Api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
 
-function WebSearchPage() {
+
+
+async function WebSearchPage({searchParams}) {
+
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  )
+
+  const data = await response.json()
+  const results = data.items
+
+  console.log(data);
   return (
-    <div>WebSearchPage</div>
+    <>
+  {results && results.map((result, index) =>{
+
+    return(
+      <h1 key={index}>{result.title}</h1>
+
+    )
+  })}
+    </>
   )
 }
 
