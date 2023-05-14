5167c3d22f93376cb32fb700fe515740993d8e52
fultonoff <fultonkilebe7@gmail.com>
2023-05-14 22:22:57 +0200

    ✅Handle errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/navigation"
 
 
 
@@ -7,10 +8,26 @@ async function WebSearchPage({searchParams}) {
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   )
 
+  if(!response){
+    throw new Error('Something went wrong')
+  }
   const data = await response.json()
   const results = data.items
 
-  console.log(data);
+  if(!results){
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Please try a different search term or go back to homepage
+
+          <Link href={'/'} className='text-blue-500'>Home</Link>
+        </p>
+      </div>
+    )
+  }
+
+
   return (
     <>
   {results && results.map((result, index) =>{
