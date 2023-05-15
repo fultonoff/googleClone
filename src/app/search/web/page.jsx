c0c3e9e2ad69c6b3ae590aa869fc295547c48523
fultonoff <fultonkilebe7@gmail.com>
2023-05-15 02:08:38 +0200

    ✅Updated seach

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,10 @@
+import WebSearchResults from "@/components/WebSearchResults"
 import Link from "next/navigation"
 
 
 
 async function WebSearchPage({searchParams}) {
-
+  await new Promise(r => setTimeout(r, 5000))
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   )
@@ -30,13 +31,7 @@ async function WebSearchPage({searchParams}) {
 
   return (
     <>
-  {results && results.map((result, index) =>{
-
-    return(
-      <h1 key={index}>{result.title}</h1>
-
-    )
-  })}
+  {results && <WebSearchResults results={data}/>}
     </>
   )
 }
