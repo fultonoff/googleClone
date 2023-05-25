d5ce0e1374ace3ac75472c603128182d1eb4939a
fultonoff <fultonkilebe7@gmail.com>
2023-05-25 17:36:10 +0200

    ✅ Image results

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import ImageSearchResults from "@/components/ImageSearchResults"
+import Link from "next/navigation"
 
-function ImageSearchPage() {
+
+
+async function ImageSearchPage({searchParams}) {
+  await new Promise(r => setTimeout(r, 5000))
+  
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image`
+  )
+
+  if(!response){
+    throw new Error('Something went wrong')
+  }
+  const data = await response.json()
+  const results = data.items
+
+  if(!results){
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Please try a different search term or go back to homepage
+
+          <Link href={'/'} className='text-blue-500'>Home</Link>
+        </p>
+      </div>
+    )
+  }
+
+
   return (
-    <div>ImageSearchPage</div>
+    <>
+  {results && <ImageSearchResults results={data}/>}
+    </>
   )
 }
 
